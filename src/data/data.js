fd28abb337c7edc701a27c17f12685b69137d019
Ivan Hill <30849044+ivan-hill@users.noreply.github.com>
2022-03-02 13:33:10 +0100

    added project

--- src/data/data.js
+++ src/data/data.js
@@ -1,14 +1,17 @@
-import ReactRental from '../images/React-Rentals1.webp'
+import ReactRental from "../images/React-Rentals1.webp";
 import ReactRental2 from "../images/React-Rentals.webp";
-import thickthighs from '../images/thickthighs.webp'
+import thickthighs from "../images/thickthighs.webp";
 import thickthighs2 from "../images/thickthighs1.webp";
 import reactIphone from "../images/react-iphone.webp";
 import reactIphone2 from "../images/react-iphone1.webp";
 import reactTesla from "../images/react-tesla.webp";
 import reactTesla2 from "../images/react-tesla1.webp";
-import one from '../images/1.jpg'
-import two from '../images/2.jpg'
-import four from '../images/4.jpg'
+import covidTracker from "../images/covid-19-tracker.webp";
+import covidTracker2 from "../images/covid-19-tracker2.webp";
+import one from "../images/1.jpg";
+import two from "../images/2.jpg";
+import three from "../images/3.jpeg";
+import four from "../images/4.jpg";
 
 export const projects = [
   {
@@ -17,7 +20,7 @@ export const projects = [
     title: "React-iPhone",
     img2: reactIphone2,
     distort: one,
-    type: "React/ Redux Toolkit",
+    type: "Ecommerce landing page using React, Redux toolkit,and React-Spring animation",
     link: "https://react-phone.xyz/",
   },
   {
@@ -26,16 +29,17 @@ export const projects = [
     title: "React-Tesla Landing Page",
     img2: reactTesla2,
     distort: two,
-    type: "React/ Redux",
+    type: "Tesla landing page clone using React and Redux",
     link: "https://react-tesla-landing-page.netlify.app/",
   },
+
   {
     id: 3,
     title: "React-Rentals",
     img: ReactRental,
     img2: ReactRental2,
     distort: four,
-    type: "React/ Styled-Components",
+    type: "Real estate website using React and Styled-Components",
     link: "https://react-rental.netlify.app",
   },
   {
@@ -44,7 +48,16 @@ export const projects = [
     img: thickthighs,
     img2: thickthighs2,
     distort: one,
-    type: "Shopify Theme",
+    type: "Actual client Plus-size boutique using Shopify Theme",
     link: "https://thickthighsmatter.shop",
+  },
+  {
+    id: 5,
+    title: "Covid-19 Tracker",
+    img: covidTracker,
+    img2: covidTracker2,
+    distort: three,
+    type: "Covid-19 tracker using React, Charts JS, Material UI, and Firebase SDK + hosting",
+    link: "https://coronavirustracker-75e09.web.app/",
   },
 ];
